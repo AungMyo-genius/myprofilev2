8178a6ab5c65b5823f2aec437111004db51aa89f
Aung Myo <59131472+AungMyo-genius@users.noreply.github.com>
2022-12-08 02:11:09 +0630

    add_dayOrNight_with_localStorageAPI

--- js/app.js
+++ js/app.js
@@ -2,9 +2,20 @@ const darkorlight = document.querySelector(".lightordarks");
 const body = document.body;
 const brandImg = document.getElementById("brand-img");
 const dorlicon = document.querySelector(".lightordarks .fas");
+const isCheck = document.querySelector(".form-check-input")
 
 
 darkorlight.addEventListener("click" , changemode);
+
+window.addEventListener('load', () => {
+    if(localStorage.getItem('status') == 'dark'){
+        changemode()
+        isCheck.checked = true;
+    }else{
+        isCheck.checked = false;
+    }
+})
+
 
 function changeAll(){
     changeText();
@@ -15,20 +26,24 @@ function changeAll(){
 }
 
 function changemode(){
-    
+
+     
+
     if(body.classList.contains("light")){
 
         document.querySelector(".navbar-light").classList.replace("navbar-light","navbar-dark");
         body.classList.remove("light");
         changeAll();
         brandImg.src = "img/brandlight.png";
-
+        localStorage.setItem("status", "light")
+        
     } else {
         
         document.querySelector(".navbar-dark").classList.replace("navbar-dark","navbar-light");
         body.classList.add("light");
         changeAll();
         brandImg.src = "img/branddark.png";
+        localStorage.setItem("status", "dark")
 
     }
 }
@@ -65,6 +80,47 @@ window.addEventListener("scroll", ()=>{
         document.querySelector(".navbar").classList.remove("smalls");
     }
 })
+// Start Type Effect
+// set up text to print, each item in array is new line
+let aText = new Array(
+   ' I am Aung Myo, Frontend Developer.', 'I want to be a Full-stack Developer.'
+    );
+let iSpeed = 100; // time delay of print out
+let iIndex = 0; // start printing array at this posision
+let iArrLength = aText[0].length; // the length of the text array
+let iScrollAt = 20; // start scrolling up at this many lines
+     
+let iTextPos = 0; // initialise text position
+let sContents = ''; // initialise contents variable
+let iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     let destination = document.querySelector(".profiletexts");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+    typewriter();
+// End Type Effect
+
+
 // Start About section
 const nowyear = new Date().getFullYear();
 const indicators = document.querySelector(".carousel-indicators");
@@ -113,6 +169,4 @@ function changecardbtn() {
         // }
 )
 }
-
-
 // End Portfolio Section
